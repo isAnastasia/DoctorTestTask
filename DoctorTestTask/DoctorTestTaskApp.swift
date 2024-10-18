06c40cca27bd5a743feeb9414a499080f6fdd519
isAnastasia <nasty04012003@gmail.com>
2024-10-18 17:25:29 +0300

    Initial Commit

---
+++ DoctorTestTask/DoctorTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorTestTaskApp.swift
+//  DoctorTestTask
+//
+//  Created by Anastasia Gorbunova on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoctorTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
